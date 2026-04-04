4cac3e34367c123ba55047620fc7c93e8ae64e5a
jussara08 <jussaraalmeidagaspar3@gmail.com>
2026-04-04 16:15:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -236,6 +236,11 @@ line_weight = st.sidebar.slider("Line thickness", 2, 10, 5)
 map_theme = st.sidebar.selectbox(
     "Map style",
     ["CartoDB positron", "CartoDB dark_matter", "OpenStreetMap"]
+    format_func=lambda x: {
+        "CartoDB positron": "Light",
+        "CartoDB dark_matter": "Dark",
+        "OpenStreetMap": "Street"
+    }[x]
 )
 
 st.sidebar.markdown("---")
